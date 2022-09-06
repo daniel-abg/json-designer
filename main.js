b13c90319d4f72a5ef21b7b4b1415ad3351f551b
Daniel <>
2022-09-06 21:56:33 +0200

    Copy the JSON string into the clipboard

--- main.js
+++ main.js
@@ -4,10 +4,20 @@ const MDCTextField = mdc.textField.MDCTextField;
 
 const textField = new MDCTextField(document.querySelector('.mdc-text-field'));
 
+// EventListeners
+document.querySelector('#copyJSON').addEventListener('click', copyJSON)
+
 /**
  * Number of spaces of intendation in the JSON string.
  */
 let space = 2;
+
+/**
+ * Copies the JSON string into the clipboard of the user.
+ */
+function copyJSON() {
+    navigator.clipboard.writeText(getJson(space));
+}
 
 /**
  * Pastes the JSON string into the textarea.
