8554e89b54577ec633a5bf682e3484d0d1415b6f
Daniel <>
2022-09-08 20:58:44 +0200

    Delete a selected key

--- json.js
+++ json.js
@@ -21,3 +21,32 @@ let json = {
 export function getJson(space) {
     return JSON.stringify(json, null, space);
 }
+
+/**
+ * @param {*} obj JavaScript object
+ * @param {*} prefix 
+ * @returns All keys from nested objects.
+ * @source https://stackoverflow.com/questions/47062922/how-to-get-all-keys-with-values-from-nested-objects
+ */
+export const keyify = (obj = json, prefix = '') =>
+    Object.keys(obj).reduce((res, el) => {
+        if (typeof obj[el] === 'object' && obj[el] !== null) {
+            return [...res, ...keyify(obj[el], prefix + el + '.')];
+        }
+        return [...res, prefix + el];
+    }, []);
+
+/**
+ * Deletes a key in the JavaScript object
+ * @param {*} path Path of the key, e.g.: book.author.firstname
+ */
+export function deleteKey(path) {
+    let keys = path.split(".");
+    let jsonObj = json;
+
+    for (let i = 0; i < keys.length - 1; i++) {
+        jsonObj = jsonObj[keys[i]];
+    }
+
+    delete jsonObj[keys[keys.length - 1]];
+}
--- main.js
+++ main.js
@@ -1,11 +1,16 @@
-import { getJson } from './json.js'
+import { getJson, keyify, deleteKey as jsonDeleteKey } from './json.js'
 
 const MDCTextField = mdc.textField.MDCTextField;
+const MDCSelect = mdc.select.MDCSelect;
 
+// The dropdown needs to be filled before the MDCSelect gets instantiated
+fillDropdownList();
+const selectKey = new MDCSelect(document.querySelector('.mdc-select-keys'));
 const textField = new MDCTextField(document.querySelector('.mdc-text-field'));
 
 // EventListeners
-document.querySelector('#copyJSON').addEventListener('click', copyJSON)
+document.querySelector('#copyJSON').addEventListener('click', copyJSON);
+document.querySelector('#deleteKey').addEventListener('click', deleteKey);
 
 /**
  * Number of spaces of intendation in the JSON string.
@@ -23,6 +28,75 @@ function copyJSON() {
  * Pastes the JSON string into the textarea.
  */
 function pasteJSON() {
-    document.getElementById("textarea").innerHTML = getJson(space);
+    textField.value = getJson(space);
 }
 pasteJSON();
+
+/**
+ * Fills the key dropdown with <li> elements
+ */
+function fillDropdownList() {
+    let ul = document.getElementById("dropdownKeys");
+    let keys = keyify();
+
+    for (const key of keys) {
+        createLiElement(ul, key);
+    }
+}
+
+/**
+ * Creates a <li> element in the key dropdown
+ * @param {*} ul <ul> element which is the key dropdown
+ * @param {*} key Path to a key, e.g.: book.author.firstname
+ */
+function createLiElement(ul, key) {
+    let li;
+    let spanRipple;
+    let spanText;
+
+    li = document.createElement("li");
+    li.className = "mdc-list-item";
+    li.setAttribute("aria-selected", "false");
+    li.setAttribute("data-value", key);
+    li.setAttribute("role", "option");
+
+    spanRipple = document.createElement("span");
+    spanRipple.className = "mdc-list-item__ripple";
+    li.appendChild(spanRipple);
+
+    spanText = document.createElement("span");
+    spanText.className = "mdc-list-item__text";
+    spanText.textContent = key;
+    li.appendChild(spanText);
+
+    ul.appendChild(li);
+}
+
+/**
+ * Deletes a <li> element in the key dropdown
+ * @param {*} key Path to a key, e.g.: book.author.firstname
+ */
+function deleteLiElement(key) {
+    selectKey.selectedIndex = -1;
+    const li = document.querySelector(`[data-value='${key}']`)
+    li.remove();
+    selectKey.layoutOptions();
+}
+
+/**
+ * Deletes a key from JSON and the key dropdown
+ */
+function deleteKey() {
+    let selectedKey = selectKey.value;
+
+    jsonDeleteKey(selectedKey);
+    deleteLiElement(selectedKey);
+    pasteJSON();
+}
+
+/**
+ * Gets executed when a new key gets selected in the dropdown
+ */
+selectKey.listen('MDCSelect:change', () => {
+    console.log(`Selected option at index ${selectKey.selectedIndex} with value "${selectKey.value}"`);
+});
