d6365e6fbf1299ee2e259eceb0e4d1bb124a85f9
Daniel Abgottspon <112648663+daniel-abg@users.noreply.github.com>
2025-01-25 21:50:02 +0100

    Refactor function

--- src/service.js
+++ src/service.js
@@ -1,33 +1,27 @@
 const ServiceModule = () => {
     /**
      * Creates an HTML element
-     * @param {*} name The name of the tags
+     * @param {*} name The name of the HTML tag
      * @param {*} classes All classes*: ["class1", "class2"]
      * @param {*} attributes All attributes*: {"attr1": "value1", "attr2": "value"}
      * @param {*} text The inner text*
      * @info *optional  (pass 'undefined')
      * @returns An HTML element
      */
-    function createHtmlElement(name, classes, attributes, text) {
-        if (name.trim() == "" || name === null || name === undefined) {
+    function createHtmlElement(name, classes = [], attributes = {}, text) {
+        if(!name) {
             return undefined;
         }
 
-        let element = document.createElement(name);
+        const element = document.createElement(name);
 
-        if (classes != undefined && classes != null) {
-            for (const className of classes) {
-                element.classList.add(className);
-            }
+        if (classes.length) {
+            element.classList.add(...classes);
         }
-
-        if (attributes != undefined && attributes != null) {
-            Object.entries(attributes).flatMap(([key, value]) => {
-                element.setAttribute(key, value);
-            });
+        for (const [key, value] of Object.entries(attributes)) {
+            element.setAttribute(key, value);
         }
-
-        if (text != undefined && text != null) {
+        if (text) {
             element.innerText = text;
         }
 
