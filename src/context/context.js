f13512f4bb0611dac4ca25b25c7e2cdc8aa58604
Daniel Abgottspon <112648663+daniel-abg@users.noreply.github.com>
2025-03-30 14:51:06 +0200

    Provide function for dispatching the event

--- src/context/context.js
+++ src/context/context.js
@@ -3,19 +3,18 @@ import { createContext } from '@lit/context';
 const jsonContext = createContext(Symbol('json-context'));
 
 const updateJson = (component, json) => {
-    const event = new CustomEvent('json-changed', {
-        bubbles: true,
-        composed: true,
-        detail: { json, hasError: false, errorMessage: '' },
-    });
-    component.dispatchEvent(event);
+    updateContext(component, { json, hasError: false, errorMessage: '' });
 };
 
 const reportError = (component, errorMessage) => {
+    updateContext(component, { hasError: true, errorMessage });
+};
+
+const updateContext = (component, value) => {
     const event = new CustomEvent('json-changed', {
         bubbles: true,
         composed: true,
-        detail: { hasError: true, errorMessage },
+        detail: value,
     });
     component.dispatchEvent(event);
 };
